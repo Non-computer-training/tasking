9f4ca6168ec0ef23c2d325fe3a4f6a44f8494db3
mahuanfy <mahuanfy@foxmail.com>
2019-12-26 10:20:08 +0800

    get student scores

---
+++ index.js
@@ -0,0 +1,55 @@
+function loadAllScore() {
+    return [{
+        name: "张三",
+        id: "TWA20190101",
+        chinese: 95,
+        english: 80,
+        math: 95,
+        programming: 80
+    },
+    {
+        name: "小明",
+        id: "TWA20190102",
+        chinese: 50,
+        english: 78,
+        math: 86,
+        programming: 56
+    },
+    {
+        name: "小红",
+        id: "TWA20190103",
+        chinese: 73,
+        english: 97,
+        math: 85,
+        programming: 88
+    },
+    {
+        name: "小黑",
+        id: "TWA20190104",
+        chinese: 66,
+        english: 63,
+        math: 73,
+        programming: 93
+    }]
+}
+
+function showStudentScores(studentIds) {
+    const studentScores = getStudentScores(studentIds);
+    console.log(studentScores);
+}
+
+function getStudentScores(studentIds) {
+    const studentInfo = loadAllScore();
+    const SUBJECT_NUMBER = 4;
+    const studentScores = studentIds.map(studentId => {
+        const findStudentInfo = studentInfo.find(student => student.id === studentId);
+        findStudentInfo.summary = findStudentInfo.chinese + findStudentInfo.english + findStudentInfo.math + findStudentInfo.programming;
+        findStudentInfo.average = findStudentInfo.summary / SUBJECT_NUMBER;
+        return findStudentInfo;
+    });
+    return studentScores;
+}
+
+const studnetIds = ["TWA20190101", "TWA20190102"]
+showStudentScores(studnetIds);
+
